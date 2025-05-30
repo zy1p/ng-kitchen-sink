d5961c6107c746aba8117b3758b1b77e9963729c
zy1p <zy1p@icloud.com>
2025-05-29 23:26:13 -0400

    chore: primeng with tailwindcss

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -24,6 +24,12 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+        },
       },
     }),
   ],
