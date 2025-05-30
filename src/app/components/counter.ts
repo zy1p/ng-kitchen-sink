dedb6cdd3cc0d48815f570509892a898ca4baea8
zy1p <zy1p@icloud.com>
2025-05-29 23:33:37 -0400

    chore: counter using primeng

--- src/app/components/counter.ts
+++ src/app/components/counter.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'counter',
+  imports: [ButtonModule],
   template: `
-    <button (click)="increment()" class="border rounded-md px-1">
-      Increment
-    </button>
-    <p>count: {{ count }}</p>
+    <p-button
+      icon="pi pi-plus"
+      severity="secondary"
+      size="small"
+      [rounded]="true"
+      (click)="increment()"
+      class=""
+    />
+    <span class="px-2"> count: {{ count() }} </span>
+    <p-button
+      icon="pi pi-minus"
+      severity="secondary"
+      size="small"
+      [rounded]="true"
+      (click)="decrement()"
+      class=""
+    />
   `,
 })
 export class Counter {
-  count = 0;
+  readonly count = signal(0);
+  constructor() {
+    effect(() => {
+      console.log(`The count is: ${this.count()}`);
+    });
+  }
 
   increment() {
-    this.count++;
+    this.count.set(this.count() + 1);
+  }
+
+  decrement() {
+    this.count.set(this.count() - 1);
   }
 }
